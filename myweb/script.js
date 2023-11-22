aff1a5abec9bfefb9b5cdf42f3854f7cf4d765f3
anzati1 <awesomeabe123@hotmail.com>
2023-11-22 10:47:06 -0500

    my web v3

--- myweb/script.js
+++ myweb/script.js
@@ -1,16 +1,16 @@
 const http = require('http');
 const fs = require('fs');
 
-http.createServer(function (req, res) {
+http.createServer(function(req, res) {
   if (req.method === 'POST') {
     let body = '';
 
-    req.on('data', function (data) {
+    req.on('data', function(data) {
       body += data;
     });
 
-    req.on('end', function () {
-      fs.writeFile('data.txt', body, function (err) {
+    req.on('end', function() {
+      fs.writeFile('data.txt', body, function(err) {
         if (err) {
           res.writeHead(500, { 'Content-Type': 'text/plain' });
           res.end('Server error');
@@ -21,7 +21,8 @@ http.createServer(function (req, res) {
       });
     });
   } else {
-    res.writeHead(400, { 'Content-Type': 'text/plain' });
-    res.end('Invalid request');
+    // Changed to return error code 405 Method Not Allowed instead of 400 Bad Request
+    res.writeHead(405, { 'Content-Type': 'text/plain', 'Allow': 'POST' });
+    res.end('Method not allowed');
   }
 }).listen(8080);
